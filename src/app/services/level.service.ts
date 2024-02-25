2b0da79e5a1af0c8da36bf7624023b57f872f9ba
CHERIEF Abdelkader <etudiant.cherief@gmail.com>
2024-02-24 20:49:47 -0500

    level service is conform with level controller in the backend

--- src/app/services/level.service.ts
+++ src/app/services/level.service.ts
@@ -15,22 +15,11 @@ export class LevelService {
     return this.http.get<Level[]>(this.apiUrl);
   }
 
-  getLevel(id: number): Observable<Level> {
-    return this.http.get<Level>(`${this.apiUrl}/id/${id}`);
-  }
-
   createLevel(Level: Level): Observable<Level> {
     return this.http.post<Level>(this.apiUrl, Level);
   }
 
   updateLevel(id: number, Level: Level): Observable<Level> {
     return this.http.put<Level>(`${this.apiUrl}/${id}`, Level);
-  }
-
-  searchLevels(searchTerm: string): Observable<Level[]> {
-    console.log('Searching for Levels with term:', searchTerm);
-    return this.http.get<Level[]>(`${this.apiUrl}/search`, {
-      params: { term: searchTerm }
-    });
   }
 }
